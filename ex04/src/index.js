509dfc1f50f1a7be3b30e4b217ddeaaa6eee0782
Ajla Karajko <ajlafi@maestralsolutions.com>
2022-09-23 10:17:42 +0200

    Repo15

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+const milili = [10, 25, 4];
+
+function myArr(mili) {
+    "use strict";
+
+    mili[0] = 4;
+    mili[1] = 10;
+    mili[2] = 25;
+
+    return mili;
+}
+
+myArr(milili);
+console.log(myArr(milili));
+
+module.exports = myArr;
